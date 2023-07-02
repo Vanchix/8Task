4aec30f34f2b1db601a84fd8d2f98aee9986a5e7
Ivan <vidukivan60@gmail.com>
2023-07-03 00:54:01 +0300

    Crating project
    Crating project

---
+++ 8Task/8Task/Animal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8Task
+{
+    internal abstract class Animal
+    {
+        public string Name;
+        public string GetName()
+        {
+            return Name;
+        }
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+        public abstract void Eat();
+    }
+}
---
+++ 8Task/8Task/Program.cs
@@ -0,0 +1,10 @@
+﻿using _8Task;
+
+Console.WriteLine("Enter dog's name");
+var name = Console.ReadLine();
+
+var dog = new Dog();
+dog.SetName(name);
+
+Console.Write($"{dog.GetName()}");
+dog.Eat();
